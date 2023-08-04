c08488bd4a5a090c2b07e4bb0a1af9af0b321f9c
조정훈 <jeongh1021@naver.com>
2023-08-04 22:13:14 +0900

    heroku deploy 커맨드 추가

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4004);
+  await app.listen(process.env.PORT || 4004);
 }
 bootstrap();
